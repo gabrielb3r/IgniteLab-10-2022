e362e6dbde68a66852c58d9beaa34441dca14f9f
GABRIEL BERNARDO MARTINS - SEMMA <T000007@pmc.local>
2022-10-19 15:45:10 -0300

    adicionado button e heading no storybook

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,9 +1,23 @@
-interface TextProps{
-    size?: 'sm' | 'md' | 'lg'
+import {Slot} from '@radix-ui/react-slot';
+import {clsx} from 'clsx';
+import {ReactNode} from 'react';
+
+export interface TextProps{
+    size?: 'sm' | 'md' | 'lg';
+    children: ReactNode;
+    asChild?: boolean;
 }
 
-export function Text({size = 'md'}: TextProps){
+export function Text({size = 'md', children, asChild}: TextProps){
+    const Comp = asChild ? Slot:'span';
     return (
-        <span className="text-gray-100 font-sans">txt</span>
+        <Comp className={clsx(
+            'text-gray-100 font-sans',
+            {
+                'text-xs' : size ==='sm',
+                'text-sm' : size ==='md',
+                'text-md' : size ==='lg',
+            }
+        )}>{children}</Comp>
     )
 }
